678d6f8be863e103510660312a4bafb08bf353db
Francisco Padilla <francisco_pc@hotmail.com>
2026-01-28 00:27:16 -0600

    Clean Folders obj & bin

---
+++ Entities/Models/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+
+        public ApiResponse(bool success, string message, T? data = default)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+    }
+}
